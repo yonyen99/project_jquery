777bffeee4b0e5506d99e8088ae31401df65e5f5
yon yen <yon.yen@pnc.lan>
2019-12-17 09:23:34 +0700

    add more js

--- js/json.js
+++ js/json.js
@@ -10,133 +10,131 @@ $(document).ready(function () {
         console.log(recipeId);
         getRecipe(recipeId);
     })
-///add 
-    $('#input').hide();
+
+    //add to number to click function
     $('#add').on('click', function () {
         var input = $('#value').val();
         userInput(input);
-        $('#input').show();
-
     })
-//low
-    $('#output').hide();
+
+    //low to minus function
     $('#low').on('click', function () {
         var input = $('#value').val();
         lowInput(input);
-        $('#output').show();
     })
-
 });
 
-function requerstApi() {
-    $.ajax({
-        dataType: 'json',
-        url: getUrl(),
-        success: (data) => chooseRecipe(data.recipes),
-        error: () => console.log('error'),
-    })
-}
-var allData = [];
-function chooseRecipe(recipe) {
-    allData = recipe;
-    var option = "";
-    recipe.forEach(element => {
-        option += `
-            <option value="${element.id}">${element.name}</option>
-        `;
-    });
-    $('#select').append(option);
-}
-function getRecipe(id) {
-    allData.forEach(item => {
-       
-        if (item.id == id) {
+        //requesApi for url
+    function requerstApi() {
+        $.ajax({
+            dataType: 'json',
+            url: getUrl(),
+            success: (data) => chooseRecipe(data.recipes),
+            error: () => console.log('error'),
+        })
+    }
+        //create array allData for using
+    var allData = [];
+        //fucntion chooseRecipe 
+    function chooseRecipe(recipe) {
+        allData = recipe;
+        var option = "";
+        recipe.forEach(element => {
+            option += `
+                <option value="${element.id}">${element.name}</option>
+            `;
+        });
+        $('#select').append(option);
+    }
+    //function getRecipe
+    function getRecipe(id) {
+        allData.forEach(item => {
+            if (item.id == id) {
+                eachRecipe(item.name, item.iconUrl);
+                ingredients(item.ingredients);
+                eachGuest(item.nbGuests);
+                instructions(item.instructions);
 
-            
-            eachRecipe(item.name, item.iconUrl);
-            ingredients(item.ingredients);
-            eachGuest(item.nbGuests);
-            instructions(item.instructions);
-           
+            }
+        })
+    }
+    //
+    function eachRecipe(name, img) {
+        var result = "";
+        result += `
+        <div class ="col-4"></div>
+        <div class ="col-4">
+            <h1>${name}</h1> 
+        </div>
+        <div class ="col-4">
+        <img src ="${img}" width ="100">
+        </div>
+        `;
+        $('#recipe-results').html(result);
+    }
+
+        //hide before see 
+    $('#show').hide();
+    //function eachGuest to get data from api
+    function eachGuest(guest) {
+        var result = "";
+        result += `
+            <input type="text" id="value" class="form-control text-center" disabled value="${guest}">
+        `;
+        $('#values').html(result);
+        //show after
+        $('#show').show();
+    }
+
+    //function ingredients to get data from ingredients
+    function ingredients(ingredients) {
+        var result_ingredients = "";
+        ingredients.forEach(element => {
+            result_ingredients += `
+            <tr>
+            <td><img src ="${element.iconUrl}" width ="40"></td>
+            <td id ="quan">${element.quantity}</td>
+            <td>${element.unit[0]}</td>
+            <td>${element.name}</td>
+            </tr>
+            `;
+        })
+        $('#ingradient').html(result_ingredients);
+    }
+    //function instructions for loop step to output
+    function instructions(step) {
+        var getStep = "";
+        var steps = step.split("<step>")
+        for (let i = 1; i < steps.length; i++) {
+            getStep += `
+                <div class="col-4"></div>
+                <div class="col-6">
+                <h5>Step ${i}</h5>
+                <p>${steps[i]}</p>
+                </div>
+                <div class="col-4"></div>
+            `;
         }
-    })
-}
-function eachRecipe(name, img) {
-    var result = "";
-    result += `
-    <div class="col-4"></div>
-    <div class="col-4">
-    <h1>${name}</h1>
-    <div class="col-4">
-    <img src ="${img}" width ="100">
-    </div>
-    `;
-    $('#recipe-results').html(result);
-}
-
-$('#show').hide();
-function eachGuest(guest) {
-    var result = "";
-    result += `
-        <input type="text" id="value" class="form-control text-center" disabled value="${guest}">
-    `;
-    $('#values').html(result);
-    $('#show').show();
-}
-
-
-function ingredients(ingredients) {
-    var result_ingredients = "";
-    ingredients.forEach(element => {
-        result_ingredients += `
-       
-        <tr>
-        <td><img src ="${element.iconUrl}" width ="40"></td>
-        <td id ="quan">${element.quantity}</td>
-        <td>${element.unit[0]}</td>
-        <td>${element.name}</td>
-        </tr>
-        `;
-
-    })
-
-    $('#ingradient').html(result_ingredients);
-}
-
-function instructions(step){
-    var getStep="";
-    var steps=step.split("<step>")
-    for(let i=1; i<steps.length; i++){
-        getStep +=`
-            <div class="col-4"></div>
-            <div class="col-6">
-            <h5>Step ${i}</h5>
-            <p>${steps[i]}</p>
-            </div>
-       
-            <div class="col-4"></div>
-        `;
+        $("#step").html(getStep);
     }
-    $("#step").html(getStep);
-}
 
-///userInput
-function userInput(values) {
-    var getValue = parseInt(values) + 1;
-    if (getValue <= 15) {
-        $('#value').val(getValue);
-        mal(getValue);
+    ///userInput to add number
+    function userInput(values) {
+        var getValue = parseInt(values) + 1;
+        if (getValue <= 15) {
+            $('#value').val(getValue);
+            mal(getValue);
+        }
     }
-}
 
-///lowInput
-function lowInput(values) {
-    var lowValue = parseInt(values) - 1;
-    if (lowValue >= 0) {
+    ///lowInput to minus number
+    function lowInput(values) {
+        var lowValue = parseInt(values) - 1;
+        if (lowValue >= 0) {
 
-        $('#value').val(lowValue);
-        mal(lowValue);
+            $('#value').val(lowValue);
+            mal(lowValue);
+        }
     }
-}
 
 
